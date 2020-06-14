f6b63c4af8b0a01eabb5b387f891c30234d02db7
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-14 15:46:06 +0530

    Update and rename deploy.py to Fake_deploy.py

--- userbot/plugins/Fake_deploy.py
+++ userbot/plugins/Fake_deploy.py
@@ -31,11 +31,11 @@ async def _(event):
 
 
 
-    await event.edit("Typing...")
+    await event.edit("Connecting to Heroku... ")
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (GujjuBot)**",
+            "**Heroku Connecting To Latest Github Build (Custom UserBot)**",
             "**Build started by user** **{DEFAULTUSER}**",
             "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
             "**Restarting Heroku Server...**",
