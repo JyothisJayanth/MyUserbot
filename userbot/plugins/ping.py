b45db9bc6e75507edf57b6d46c05aa74370547a2
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-28 21:32:22 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -10,4 +10,4 @@ async def _(event):
     await event.edit("oooof!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("King Kong plays Ping Pong!\n{}".format(ms))
+    await event.edit("King Kong🦍 plays Ping Pong!🏓\n{}".format(ms))
