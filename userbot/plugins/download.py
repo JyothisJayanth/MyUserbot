69e34e9dbe844d8355de94dd008aead2e4beae55
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-28 20:14:54 +0530

    Update download.py

--- userbot/plugins/download.py
+++ userbot/plugins/download.py
@@ -1,7 +1,7 @@
 """Download Files to your local server
 Syntax:
-.dl
-.dl url | file.name to download files from a Public Link"""
+.dload
+.dload url | file.name to download files from a Public Link"""
 
 import aiohttp
 import asyncio
@@ -15,7 +15,7 @@ from telethon.tl.types import DocumentAttributeVideo
 from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 
 
-@borg.on(admin_cmd(pattern="dl ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="dload ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
