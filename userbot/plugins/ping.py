63b47dbef381a036432fd8178b91c28dd454b393
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-30 17:53:53 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,13 +1,53 @@
+# special thanks to Sur_vivor 
+# first on friday by leobrownlee 
 from telethon import events
 from datetime import datetime
+from userbot.utils import admin_cmd
+from userbot.__init__ import StartTime
+import time
 
 
-@command(pattern="^.ping")
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+
+
+
+
+
+#@command(pattern="^.ping$")
+@borg.on(admin_cmd(pattern="ping$"))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("oooof!")
+    await event.edit("oof!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("King Kong🦍 plays Ping Pong!🏓\n{}".format(ms))
+    uptime = get_readable_time((time.time() - StartTime))
+    await event.edit(f"🏓Ping speed: {ms}\nUptime: {uptime}")
