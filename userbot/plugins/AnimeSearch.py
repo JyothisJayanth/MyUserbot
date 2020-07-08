4dc4b7e7141f27d810484a8b97c0b08009fef3b3
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-08 19:33:24 +0530

    Update AnimeSearch.py

--- userbot/plugins/AnimeSearch.py
+++ userbot/plugins/AnimeSearch.py
@@ -35,3 +35,25 @@ async def _(event):
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
+
+@borg.on(admin_cmd(pattern="ani ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    input_str = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    chat = "@AniFluidbot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1072580256))
+              await event.client.send_message(chat, "/anime {}".format(input_str))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Master! Please Unblock (@AniFluidbot) ```")
+              return
+          if response.text.startswith("Country"):
+             await event.edit("😶**Country Not Found**😅\n\n[🔴🔴🔴🔴\n ⏩⏩ How to use ⏪⏪\n🔵🔵🔵🔵](https://t.me/Dev_OwO)")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
