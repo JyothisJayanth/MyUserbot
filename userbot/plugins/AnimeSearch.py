28b8f938e83bde8c63cb6de5848faf515db3c844
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-08 19:12:35 +0530

    Update AnimeSearch.py

--- userbot/plugins/AnimeSearch.py
+++ userbot/plugins/AnimeSearch.py
@@ -19,7 +19,7 @@ async def _(event):
         return    
     sysarg = event.pattern_match.group(1)
 
-      async with borg.conversation(bot) as conv:
+      async with event.client.conversation(bot) as conv:
           try:
               await conv.send_message("/start")
               response = await conv.get_response()
@@ -29,7 +29,31 @@ async def _(event):
               await borg.send_message(event.chat_id, result.text)
  
               await event.delete()
- 
+
+
+ from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="urband ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    input_str = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    chat = "@KeikoSDbot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1212429864))
+              await event.client.send_message(chat, "/ud {}".format(input_str))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Master! Please Unblock (@KeikoSDbot) ```")
+              return
+          if response.text.startswith("Country"):
+             await event.edit("😶**Country Not Found**😅\n\n[🔴🔴🔴🔴\n ⏩⏩ How to use ⏪⏪\n🔵🔵🔵🔵](https://t.me/Dev_OwO)")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
 
 @borg.on(admin_cmd("scharacter ?(.*)"))
  
@@ -41,7 +65,7 @@ async def _(event):
  
     sysarg = event.pattern_match.group(1)
  
-      async with borg.conversation(bot) as conv:
+      async with event.client.conversation(bot) as conv:
  
           try:
  
