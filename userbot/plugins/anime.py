aecff711e526464eb484177f002eabcffe5ab1fd
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-10 22:15:43 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -130,32 +130,6 @@ async def _(event):
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
 
-################################
-
-
-@borg.on(admin_cmd(pattern="sanime ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    reply_message = await event.get_reply_message()
-    chat = "@NepgearBot"
-    await event.edit("```Checking...```")
-    async with event.client.conversation(chat) as conv:
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1072580256))
-              await event.client.send_message(chat, "/anime {}".format(input_str))
-              response = await response
-          except YouBlockedUserError:
-              await event.reply("```Master! Please Unblock (@NepgearBot) ```")
-              return
-          if response.text.startswith("🕒"):
-             await event.edit("**Fetching Details..**")
-             await event.client.send_message(event.chat_id, response.message)
-          else:
-             await event.delete()
-             response = await response
-             await event.client.send_message(event.chat_id, response.message)
 
 #####################################
 
