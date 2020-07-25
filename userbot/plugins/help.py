d026c6a4e7ddfc0843c513b4b263593f82642a6f
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-25 15:01:55 +0530

    test with help mod

---
+++ userbot/plugins/help.py
@@ -0,0 +1,21 @@
+"""Userbot help command"""
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.chelp(?: |$)(.*)")
+async def help(event):
+    """For .chelp command"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("Please specify a valid module name.")
+    else:
+        string = "Specify which module do you want help for !!\n**Usage:** `.chelp` <module name>\n\n"
+        for i in sorted(CMD_HELP):
+            string += "`" + str(i) + "`"
+            string += "\t\t\t||\t\t\t "
+        await event.edit(string)
