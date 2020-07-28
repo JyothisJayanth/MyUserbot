f5a212650eca565e53d67a6d0628969ea1c8999a
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-28 14:47:22 +0530

    Revert "Update pmpermit_sql.py"
    This reverts commit d509706fdb6087da0858cace57e2718b1db2a135.

--- userbot/plugins/sql_helper/pmpermit_sql.py
+++ userbot/plugins/sql_helper/pmpermit_sql.py
@@ -1,35 +1,43 @@
-from sqlalchemy import Column, String, Numeric
+from sqlalchemy import Column, String
 from userbot.plugins.sql_helper import SESSION, BASE
 
 
-class NOLogPMs(BASE):
-    __tablename__ = "no_log_pms"
-    chat_id = Column(Numeric, primary_key=True)
+class PMPermit(BASE):
+    __tablename__ = "pmpermit"
+    chat_id = Column(String(14), primary_key=True)
+    reason = Column(String(127))
 
     def __init__(self, chat_id, reason=""):
         self.chat_id = chat_id
+        self.reason = reason
 
 
-NOLogPMs.__table__.create(checkfirst=True)
+PMPermit.__table__.create(checkfirst=True)
 
 
 def is_approved(chat_id):
     try:
-        return SESSION.query(NOLogPMs).filter(NOLogPMs.chat_id == chat_id).one()
+        return SESSION.query(PMPermit).filter(PMPermit.chat_id == str(chat_id)).one()
     except:
         return None
     finally:
         SESSION.close()
 
 
-def approve(chat_id):
-    adder = NOLogPMs(chat_id)
+def approve(chat_id, reason):
+    adder = PMPermit(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
 def disapprove(chat_id):
-    rem = SESSION.query(NOLogPMs).get(chat_id)
+    rem = SESSION.query(PMPermit).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
+
+
+def get_all_approved():
+    rem = SESSION.query(PMPermit).all()
+    SESSION.close()
+    return rem
