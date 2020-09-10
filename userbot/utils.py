335a6234ce3aca4328c8ef2eaed094bc8181d6e9
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-09-10 20:00:04 +0530

    smol fix

--- userbot/utils.py
+++ userbot/utils.py
@@ -120,7 +120,7 @@ def load_module(shortname):
         sys.modules["userbot.plugins."+shortname] = mod
         print("Successfully (re)imported "+shortname)
 
-            bot.tgbot = TelegramClient(
+            tgbot = TelegramClient(
                 "TGBOT_TOKEN",
                 api_id=APP_ID,
                 api_hash=API_HASH
