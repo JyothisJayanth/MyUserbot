3c8865588a644367f73c6c292d9cfabd7e3352ef
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-09-10 19:36:33 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -105,7 +105,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.bot = bot
-        mod.tgbot = self.tgbot
+        mod.tgbot = bot.tgbot
         mod.Var = Var
         mod.command = command
         mod.logger = logging.getLogger(shortname)
@@ -120,7 +120,7 @@ def load_module(shortname):
         sys.modules["userbot.plugins."+shortname] = mod
         print("Successfully (re)imported "+shortname)
 
-            self.tgbot = TelegramClient(
+            bot.tgbot = TelegramClient(
                 "TGBOT_TOKEN",
                 api_id=APP_ID,
                 api_hash=API_HASH
